26fbb275ef9475400a9fb09d45e74a4e47e1174e
Mr. Raghav Kumar Singh <104582254+19csd26@users.noreply.github.com>
2024-01-24 04:17:47 +0530

    Create destruct Function.php

---
+++ OOPs/destruct Function.php
@@ -0,0 +1,19 @@
+/*
+A destructor is called when the object is destructed or the script is stopped or exited.
+If you create a __destruct() function, PHP will automatically call this function at the end of the script.
+*/
+<?php
+class Fruit {
+  public $name;
+  public $color;
+
+  function __construct($name) {
+    $this->name = $name;
+  }
+  function __destruct() {
+    echo "The fruit is {$this->name}.";
+  }
+}
+//creating Instance
+$apple = new Fruit("Apple");
+?>
